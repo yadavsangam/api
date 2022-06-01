af11cd4f1563b88118bbd56ba044ea30086191ea
Sangam Yadav <sangam15595@mastek.com>
2022-06-01 15:20:07 +0530

    first commit

---
+++ api.js
@@ -0,0 +1,20 @@
+const client = require('./connection.js')
+const express = require('express');
+const app = express();
+
+app.listen(3300, ()=>{
+    console.log("Sever is now listening at port 3000");
+})
+
+client.connect();
+
+
+
+app.get('/products', (req, res)=>{
+    client.query(`Select * from products`, (err, result)=>{
+        if(!err){
+            res.send(result.rows);
+        }
+    });
+    client.end;
+})
